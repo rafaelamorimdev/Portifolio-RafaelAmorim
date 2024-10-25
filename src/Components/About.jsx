1c3977f38f7fcde75303be4a574e6bc51aac31fa
Rafael <rafael.ceb2010@gmail.com>
2024-10-24 23:52:06 -0300

    alt11

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,13 +5,7 @@ export function About () {
     className="section">
         <div className="container">
             <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
-                <p className="text-zinc-300 mb-4 md:mb-8 mb:text-xl md:max-w-[120ch]">Olá! Meu nome é Rafael Amorim, um desenvolvedor web apaixonado por criar experiências digitais cativantes e de alto desempenho. Com uma combinação equilibrada de criatividade e expertise técnica, eu transformo a visão do cliente em uma obra-prima digital que se destaca tanto em aparência quanto em funcionalidade. <br/>
-
-Meu objetivo é desenvolver soluções web que não apenas atendam, mas superem as expectativas do usuário.Eu construo interfaces responsivas e altamente interativas, priorizando a usabilidade e acessibilidade.
-<br/>
-Estou constantemente me atualizando sobre as últimas tendências e tecnologias do mercado, garantindo que meu trabalho seja sempre alinhado com as melhores práticas do desenvolvimento front-end.
-<br/>
-Convido você a explorar meu portfólio e ver exemplos do meu trabalho. Ficarei feliz em discutir como posso contribuir para o sucesso do seu próximo projeto web.</p>
+                <p className="text-zinc-300 mb-4 md:mb-8 mb:text-xl md:max-w-[120ch]"> Olá! Sou Rafael Amorim, desenvolvedor front-end apaixonado por criar experiências digitais atraentes e de alto desempenho. Transformo visões em obras digitais elegantes e funcionais, focando em interfaces responsivas e interativas que superem expectativas, sempre com usabilidade e acessibilidade. Atualizado com tendências do mercado, sigo as melhores práticas no desenvolvimento front-end. Confira meu portfólio e entre em contato para discutir seu próximo projeto web.</p>
                 <div className="flex flex-wrap items-center gap-4 md:gap-7 ">
                     
                 <img 
