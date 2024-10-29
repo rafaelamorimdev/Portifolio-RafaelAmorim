54cbfb3e162aa30e507f070394482f2868200bb5
Rafael <rafael.ceb2010@gmail.com>
2024-10-29 20:14:16 -0300

    alt19

--- src/Currículo/Document.jsx
+++ src/Currículo/Document.jsx
@@ -1,31 +1,42 @@
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
-import { App } from "../App";
+import { initializeApp } from "firebase/app";
+import { getDownloadURL,getStorage, ref } from "firebase/storage";
 
-const storage = getStorage (App)
-    const DownloadPDF = () => {
-        const handleDownload = () => {
-            const pdfRef = ref(storage, 'CvRafaelAmorim.pdf');
-    
-            getDownloadURL(pdfRef)
-                .then((url) => {
-                    // Criar um link para download
-                    const link = document.createElement('a');
-                    link.href = url;
-                    link.download = 'CvRafaelAmorim.pdf';
-                    document.body.appendChild(link);
-                    link.click();
-                    document.body.removeChild(link);
-                })
-                .catch((error) => {
-                    console.error("Erro ao obter a URL de download:", error);
-                });
-        };
-    
-        return (
-            <button onClick={handleDownload}>
-                Download CV
-            </button>
-        );
+const firebaseConfig = {
+    apiKey: "AIzaSyCwea0ZIkplhXxQpKqBImrAvkOdMsuAQdk",
+    authDomain: "portifoliorafaamorim.firebaseapp.com",
+    projectId: "portifoliorafaamorim",
+    storageBucket: "portifoliorafaamorim.appspot.com",
+    messagingSenderId: "168353822866",
+    appId: "1:168353822866:web:7782afe9e3158956584e4b"
+};
+
+
+const app = initializeApp(firebaseConfig)
+const storage = getStorage(app)
+const DownloadButton = () => {
+    const handleDownload = () => {
+        const pdfRef = ref(storage, 'CvRafaelAmorim.pdf');
+
+        getDownloadURL(pdfRef)
+            .then((url) => {
+                // Criar um link para download
+                const link = document.createElement('a');
+                link.href = url;
+                link.download = 'CvRafaelAmorim.pdf';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            })
+            .catch((error) => {
+                console.error("Erro ao obter a URL de download:", error);
+            });
     };
-    
-    export default DownloadPDF;
+
+    return (
+        <button onClick={handleDownload}>
+            Download CV
+        </button>
+    );
+};
+
+export default DownloadButton;
