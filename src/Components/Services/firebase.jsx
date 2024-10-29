3d507be2c98bb46b0f06fad43a15eef1afa05181
Rafael <rafael.ceb2010@gmail.com>
2024-10-29 19:46:07 -0300

    alt17

--- src/Components/Services/firebase.jsx
+++ src/Components/Services/firebase.jsx
@@ -26,7 +26,7 @@
                     link.href = url;
                     link.download = 'CvRafaelAmorim.pdf'; 
                     document.body.appendChild(link);
-                    link.click();
+                    link.click({DownloadPDF});
                     document.body.removeChild(link);
                 })
                 .catch((error) => {
